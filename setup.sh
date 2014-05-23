acb78fdf0af08dc007ed875db4cc0ce76763a1e2
Tim Sutton <tim@linfiniti.com>
2014-05-23 15:03:22 +0200

    Config file updates  - still have permission denied issue with uwsgi and dango project

--- setup.sh
+++ setup.sh
@@ -14,6 +14,11 @@ cd /etc/supervisor/conf.d/
 ln -s /home/web/server-conf/supervisor-nginx.conf .
 ln -s /home/web/server-conf/supervisor-uwsgi.conf .
 
+# nginx config - don't daemonize
+sed -i -e"s/keepalive_timeout\s*65/keepalive_timeout 2/" /etc/nginx/nginx.conf
+sed -i -e"s/keepalive_timeout 2/keepalive_timeout 2;\n\tclient_max_body_size 100m/" /etc/nginx/nginx.conf
+echo "daemon off;" >> /etc/nginx/nginx.conf
+
 cd /home/web
 virtualenv venv
 venv/bin/pip install -r REQUIREMENTS.txt
--- start.sh
+++ start.sh
@@ -8,9 +8,5 @@ ROOT_PASSWORD=`pwgen -c -n -1 12`
 echo "root:$ROOT_PASSWORD" | chpasswd
 echo "root login password: $ROOT_PASSWORD"
 
-# Also echo out the pg password written to
-# /PGPASSWORD.txt when the image was made
-# by start-postgres.sh
-
 # Launch supervisor
 supervisord -n
