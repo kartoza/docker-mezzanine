3f4ab14121b80e6c292158189888ff98748b6bed
Tim Sutton <tim@linfiniti.com>
2014-05-24 19:53:01 +0200

    Added more notes tostart.sh

--- start.sh
+++ start.sh
@@ -2,6 +2,12 @@
 
 # Put any tasks you would like to have carried
 # out when the container is first created here
+# We do this below because the django_project
+# is copied over in a separate docker fs layer
+# and so we can't assign www-data write permissions
+# to it. So we make a throw away copy in /tmp
+# and copy it on first run so that www-data cane
+# access the copy...
 cp -r /tmp/django_project /home/web/
 chown -R www-data.www.data /home/web/django_project
 
