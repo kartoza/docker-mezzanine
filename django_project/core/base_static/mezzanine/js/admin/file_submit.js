f666de0a2296dc461e82ec3aeaa5b63c209f14ad
dimasciput <dimas.ciputra@gmail.com>
2016-08-29 18:23:14 +0700

    Fix admin pages

--- django_project/core/base_static/mezzanine/js/admin/file_submit.js
+++
@@ -1,9 +0,0 @@
-function FileSubmit(FileURL, MediaFileURL) {
-    parentWin = (!window.frameElement && window.dialogArguments) || opener || parent || top;
-    parentWin.document.getElementById("id_mugshot").value = FileURL;
-    parentWin.document.getElementById("image_id_mugshot").src = MediaFileURL;
-    parentWin.document.getElementById("help_id_mugshot").style.display = "inline";
-    parentWin.document.getElementById("link_id_mugshot").style.display = "inline";
-    parentWin.document.getElementById("clear_id_mugshot").style.display = "inline";
-    window.close();
-}
--- django_project/core/settings/project.py
+++ django_project/core/settings/project.py
@@ -45,3 +45,22 @@ MIDDLEWARE_CLASSES = (
     # Add any additional middleware classes here
 ) + MIDDLEWARE_CLASSES
 
+DATABASES = {}
+
+####################
+# DYNAMIC SETTINGS #
+####################
+
+# set_dynamic_settings() will rewrite globals based on what has been
+# defined so far, in order to provide some better defaults where
+# applicable. We also allow this settings module to be imported
+# without Mezzanine installed, as the case may be when using the
+# fabfile, where setting the dynamic settings below isn't strictly
+# required.
+try:
+    from mezzanine.utils.conf import set_dynamic_settings
+except ImportError:
+    pass
+else:
+    set_dynamic_settings(globals())
+
