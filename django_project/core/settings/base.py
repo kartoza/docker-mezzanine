cf311346b40da5ef62d8ec50af33faf8e106d94f
Tim Sutton <tim@kartoza.com>
2016-08-30 14:42:51 +0200

    Make the mezzanine_slides app work and removed gallery from the front page

--- django_project/core/settings/base.py
+++ django_project/core/settings/base.py
@@ -67,7 +67,7 @@ STATICFILES_DIRS = (
     # Put strings here, like "/home/html/static" or "C:/www/django/static".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
-    absolute_path('core', 'base_static'),
+    # absolute_path('core', 'basstatic'),
 )
 
 # List of finder classes that know how to find static files in
