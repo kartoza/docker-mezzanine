3843943b2115531a422eba7c574d6806bc968886
meomancer <meomancer@gmail.com>
2016-11-17 12:12:05 +0700

    fixing deployment

--- django_project/core/settings/dev.py
+++ django_project/core/settings/dev.py
@@ -7,6 +7,8 @@ TEMPLATE_DEBUG = DEBUG
 LOGGING_OUTPUT_ENABLED = DEBUG
 LOGGING_LOG_SQL = DEBUG
 
+ALLOWED_HOSTS = ['*']
+
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 
 # Disable caching while in development
