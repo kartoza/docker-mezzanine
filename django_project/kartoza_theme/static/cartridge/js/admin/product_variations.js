d4a369cd4c2e1bb03406c2b9200160c2d894e75e
meomancer <meomancer@gmail.com>
2016-12-09 14:10:18 +0700

    showing variant although there just one variant

---
+++ django_project/kartoza_theme/static/cartridge/js/admin/product_variations.js
@@ -0,0 +1,48 @@
+jQuery(function ($) {
+
+    var grappelli = $('.admin-title').length == 1;
+
+    // Move the "create variations" fieldset to under current variations.
+    var variationsFieldset;
+    if (grappelli) {
+        variationsFieldset = $('#id_variations-INITIAL_FORMS').parent();
+    } else {
+        variationsFieldset = $('#id_variations-INITIAL_FORMS').parent().parent();
+    }
+    $('.create-variations').insertAfter(variationsFieldset);
+    // Hide empty option fields.
+    $('.create-variations .form-row').each(function (i, row) {
+        row = $(row);
+        if (row.find('li').length == 0) {
+            row.hide();
+        }
+    });
+
+    // Deselect default variation when another is selected as default.
+    var variationDefaults = variationsFieldset.find(' .default input:checkbox');
+    variationDefaults.click(function () {
+        var clicked = $(this);
+        if (clicked.attr('checked')) {
+            $.each(variationDefaults, function (i, variation) {
+                variation = $(variation);
+                if (variation.attr('name') != clicked.attr('name')) {
+                    variation.attr('checked', false);
+                }
+            });
+        }
+    });
+
+    // Grappelli removes the string value of inline objects, so show these
+    // for the product variations.
+    if (grappelli) {
+        $.each(variationsFieldset.find('h3'), function (i, variation) {
+            variation = $(variation);
+            var titleText = variation.html().split('</b>')[1];
+            var titleHtml = '<div class="tiny">' + titleText + '</div>';
+            variation.parent().find('.sku input').before(titleHtml);
+        });
+    }
+
+    $('.variations-help').insertAfter('.create-variations h2').show();
+
+});
