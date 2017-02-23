bc67fb5a96a26063168e96a449b10a8b95ad5360
Dimas Ciputra <dimas.ciputra@gmail.com>
2017-02-23 12:24:15 +0700

    Fix template in foss4g (#40)

--- django_project/core/urls.py
+++ django_project/core/urls.py
@@ -68,6 +68,12 @@ urlpatterns = i18n_patterns(
 
     url("^$", "mezzanine.pages.views.page", {"slug": "/"}, name="home"),
 
+    # TEMPLATE HOMEPAGE FOR FOSS4G
+    # ---------------------------
+    # This pattern simply loads the index.html template.
+
+    url("^foss4g-home/", direct_to_template, {"template": "foss4g-index.html"}, name="foss4g-home"),
+
     # HOMEPAGE FOR A BLOG-ONLY SITE
     # -----------------------------
     # This pattern points the homepage to the blog post listing page,
@@ -109,8 +115,6 @@ urlpatterns = i18n_patterns(
 
     # For mezzanine-agenda
     ("^%s/" % settings.EVENT_SLUG, include("mezzanine_agenda.urls")),
-
-    #symposion url
 )
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
@@ -130,81 +134,10 @@ urlpatterns += (
     url(r"^teams/", include("symposion.teams.urls")),
 )
 
-#cartridge_url
 urlpatterns += [
-
     # Cartridge URLs.
     url("^shop/", include("cartridge.shop.urls")),
     url("^account/orders/$", order_history, name="shop_order_history"),
-
-    # We don't want to presume how your homepage works, so here are a
-    # few patterns you can use to set it up.
-
-    # HOMEPAGE AS STATIC TEMPLATE
-    # ---------------------------
-    # This pattern simply loads the index.html template. It isn't
-    # commented out like the others, so it's the default. You only need
-    # one homepage pattern, so if you use a different one, comment this
-    # one out.
-
-    url("^$", direct_to_template, {"template": "index.html"}, name="home"),
-
-    # HOMEPAGE AS AN EDITABLE PAGE IN THE PAGE TREE
-    # ---------------------------------------------
-    # This pattern gives us a normal ``Page`` object, so that your
-    # homepage can be managed via the page tree in the admin. If you
-    # use this pattern, you'll need to create a page in the page tree,
-    # and specify its URL (in the Meta Data section) as "/", which
-    # is the value used below in the ``{"slug": "/"}`` part.
-    # Also note that the normal rule of adding a custom
-    # template per page with the template name using the page's slug
-    # doesn't apply here, since we can't have a template called
-    # "/.html" - so for this case, the template "pages/index.html"
-    # should be used if you want to customize the homepage's template.
-    # NOTE: Don't forget to import the view function too!
-
-    # url("^$", mezzanine.pages.views.page, {"slug": "/"}, name="home"),
-
-    # HOMEPAGE FOR A BLOG-ONLY SITE
-    # -----------------------------
-    # This pattern points the homepage to the blog post listing page,
-    # and is useful for sites that are primarily blogs. If you use this
-    # pattern, you'll also need to set BLOG_SLUG = "" in your
-    # ``settings.py`` module, and delete the blog page object from the
-    # page tree in the admin if it was installed.
-    # NOTE: Don't forget to import the view function too!
-
-    # url("^$", mezzanine.blog.views.blog_post_list, name="home"),
-
-    # MEZZANINE'S URLS
-    # ----------------
-    # ADD YOUR OWN URLPATTERNS *ABOVE* THE LINE BELOW.
-    # ``mezzanine.urls`` INCLUDES A *CATCH ALL* PATTERN
-    # FOR PAGES, SO URLPATTERNS ADDED BELOW ``mezzanine.urls``
-    # WILL NEVER BE MATCHED!
-
-    # If you'd like more granular control over the patterns in
-    # ``mezzanine.urls``, go right ahead and take the parts you want
-    # from it, and use them directly below instead of using
-    # ``mezzanine.urls``.
-    url("^", include("mezzanine.urls")),
-
-    # MOUNTING MEZZANINE UNDER A PREFIX
-    # ---------------------------------
-    # You can also mount all of Mezzanine's urlpatterns under a
-    # URL prefix if desired. When doing this, you need to define the
-    # ``SITE_PREFIX`` setting, which will contain the prefix. Eg:
-    # SITE_PREFIX = "my/site/prefix"
-    # For convenience, and to avoid repeating the prefix, use the
-    # commented out pattern below (commenting out the one above of course)
-    # which will make use of the ``SITE_PREFIX`` setting. Make sure to
-    # add the import ``from django.conf import settings`` to the top
-    # of this file as well.
-    # Note that for any of the various homepage patterns above, you'll
-    # need to use the ``SITE_PREFIX`` setting as well.
-
-    # url("^%s/" % settings.SITE_PREFIX, include("mezzanine.urls"))
-
 ]
 
 # Adds ``STATIC_URL`` to the context of error pages, so that error
