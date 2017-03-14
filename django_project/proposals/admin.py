657b3d7208ab61641549308f4d33a7a46effe433
Muhammad Anis <anisiconic@gmail.com>
2017-03-14 10:51:55 +0700

    Add admin menu for Map and Workshop

--- django_project/proposals/admin.py
+++ django_project/proposals/admin.py
@@ -1,6 +1,10 @@
 from django.contrib import admin
 
 from .models import TalkProposal
+from .models import MapProposal
+from .models import WorkshopProposal
 
 
 admin.site.register(TalkProposal)
+admin.site.register(MapProposal)
+admin.site.register(WorkshopProposal)
