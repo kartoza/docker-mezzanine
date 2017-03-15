9a321fa8931f26d4d6d0927db8754840ee38c5ae
Muhammad Anis <anisiconic@gmail.com>
2017-03-15 12:32:20 +0700

    Fix send email template

---
+++ django_project/pinaxcon_theme/hooks.py
@@ -0,0 +1,12 @@
+from account.hooks import AccountDefaultHookSet
+from django.core.mail import send_mail
+from django.template.loader import render_to_string
+from account.conf import settings
+
+
+class Foss4GAccountHookset(AccountDefaultHookSet):
+    def send_invitation_email(self, to, ctx):
+        subject = render_to_string("account/email/email_confirmation_subject.txt", ctx)
+        subject = "".join(subject.splitlines())  # remove superfluous line breaks
+        message = render_to_string("account/email/email_confirmation_message.txt", ctx)
+        send_mail(subject, message, settings.DEFAULT_FROM_EMAIL, to)
