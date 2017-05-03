f745d9df12669f257db549d6217409c044a55a3a
ann26 <anita.hapsari15@alumni.imperial.ac.uk>
2017-05-03 16:35:45 +0700

    add newline at the end of file

--- django_project/core/settings/project.py
+++ django_project/core/settings/project.py
@@ -99,4 +99,5 @@ SHOP_CURRENCY_LOCALE = "en_ZA.UTF-8"
 ACCOUNT_HOOKSET = "core.adapter.PinaxAccountHookset"
 CONFERENCE_DOMAIN = "https://foss4g-africa.org"
 
+# default login url
 LOGIN_URL = '/account/login/'
