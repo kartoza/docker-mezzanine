555e2b2af7d9b042ef26dee05f2ebf6e81519ad8
Christiaan van der Merwe <Christiaan@Kartoza.com>
2019-10-28 10:11:34 +0200

    Added certificate admin order (#306)
    * Added certificate option to contrib.py
    
    * Added admin order

--- django_project/core/settings/contrib.py
+++ django_project/core/settings/contrib.py
@@ -189,7 +189,7 @@ SHOP_PAYMENT_STATUS_CHOICES = (
 # Sequence of indexes from the SHOP_OPTION_TYPE_CHOICES setting that
 # control how the options should be ordered in the admin,
 # eg for "Colour" then "Size" given the above:
-SHOP_OPTION_ADMIN_ORDER = (1, 2, 3, 4, 5, 6)
+SHOP_OPTION_ADMIN_ORDER = (1, 2, 3, 4, 5, 6, 7)
 
 ######################
 # MEZZANINE SETTINGS #
